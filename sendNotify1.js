572d5964c56c9d878d9d52faf740d33e43ca377d
funaihui <funaihui@lollitech.com>
2023-07-31 09:15:40 +0800

    2.2.3

--- sendNotify1.js
+++ sendNotify1.js
@@ -211,9 +211,12 @@ async function sendNotify(
     text,
     desp,
     params = {},
-    author = '\n\n' + process.env.pushDesc || '关注飞机频道：https://t.me/tigerorrose，及时获取脚本更新信息',
+    author = '\n\n关注飞机频道：https://t.me/tigerorrose，及时获取脚本更新信息',
 ) {
     //提供6种通知
+    if (process.env.pushDesc){
+        author = '\n\n' + process.env.pushDesc;
+    }
     desp += author; //增加作者信息，防止被贩卖等
     await Promise.all([
         // serverNotify(text, desp), //微信server酱
